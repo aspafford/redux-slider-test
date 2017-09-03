e78e185f092125d7a815959b4adb88f16179a797
Aaron Spafford <aaron.spafford@gmail.com>
2017-09-03 13:03:30 -0700

    load from api, debounce action

--- client/index.js
+++ client/index.js
@@ -2,22 +2,20 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { applyMiddleware, compose, createStore } from 'redux'
 import { connect, Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import axios from 'axios'
+import promise from 'redux-promise-middleware'
 import logger from 'redux-logger'
+import createDebounce from 'redux-debounced'
 
-const INITIAL_VALUE = {
-  num: 0
-}
-
-const INITIAL_STATE = [
-  INITIAL_VALUE,
-  INITIAL_VALUE,
-  INITIAL_VALUE,
-  INITIAL_VALUE
-]
+const INITIAL_STATE = []
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "DOIT": {
+    case "SLIDERS_FULFILLED": {
+      return action.payload.data
+    }
+    case "DOIT":{
       return state.map((item, index) => {
         if (index === action.index) {
           return {...item, num: action.payload}
@@ -31,14 +29,10 @@ const reducer = (state = INITIAL_STATE, action) => {
 }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
-  applyMiddleware(logger)
-));
 
-store.dispatch({type: "DOIT", index: 0, payload: 1})
-store.dispatch({type: "DOIT", index: 1, payload: 9})
-store.dispatch({type: "DOIT", index: 3, payload: 91})
-store.dispatch({type: "DOIT", index: 2, payload: 71})
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+  applyMiddleware(createDebounce(), promise(), thunk, logger)
+));
 
 store.subscribe(() => {
   // console.log(store.getState())
@@ -63,6 +57,27 @@ const sliderAction = (event, index) => {
   }
 }
 
+export const updateSlider = (index) => {
+  return {
+    type: "DOIT2",
+    index: index,
+    payload: "yar"
+  }
+}
+
+const delayedEvent = (index) => {
+  const thunk = (dispatch, getState) => {
+    dispatch(updateSlider(index))
+  }
+  thunk.meta = {
+    debounce: {
+      time: 2000,
+      key: 'UPDATE_SLIDER'
+    }
+  }
+  return thunk
+}
+
 const mapStateToProps = state => ({
   sliders: state
 })
@@ -73,6 +88,7 @@ const mapDispatchProperties =
       return {
         sliderAction: event => {
           dispatch(sliderAction(event, index));
+          dispatch(delayedEvent(index))
         }
       }
     }
@@ -88,6 +104,15 @@ const MappedSliders = props =>
 const SliderCollection = connect(mapStateToProps)(MappedSliders)
 
 class App extends Component {
+
+  componentWillMount() {
+    // promise dispatches FOO_PENDING, FOO_REJECTED, FOO_FULILLED
+    store.dispatch({
+      type: 'SLIDERS',
+      payload: axios.get('http://rest.learncode.academy/api/jobill/sliders')
+    })
+  }
+
   render() {
     return (
       <Provider store={store}>
